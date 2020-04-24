6bde9006a1adbb2b026dc83ec08dfc0724ac08d5
brennop <brennopcordeiro@gmail.com>
2020-04-24 17:33:42 -0300

    ⭐ Implements alternative wrapping mode
    This wrapping mode is accurate, but may hurt performance

--- objects/Segment.lua
+++ objects/Segment.lua
@@ -15,23 +15,11 @@ function Segment:draw()
   local modX, modY = getModular(self.body)
   local modPoints = {getPoints(modX, modY, self.size/2, self.body:getAngle())}
 
-  for i=1,4 do
-    if (modPoints[2*i-1] / love.graphics:getWidth()) > (modX / love.graphics:getWidth()) then
-      love.graphics.polygon("fill", getPoints(modX - love.graphics:getWidth(), modY, self.size/2, self.body:getAngle()))
-    elseif (modPoints[2*i-1] / love.graphics:getWidth()) < (modX / love.graphics:getWidth()) then
-      love.graphics.polygon("fill", getPoints(modX + love.graphics:getWidth(), modY, self.size/2, self.body:getAngle()))
+  for i=-1,1 do
+    for j=-1,1 do
+      love.graphics.polygon("fill", getPoints(modX + i * love.graphics:getWidth(), modY + j * love.graphics:getHeight(), self.size/2, self.body:getAngle()))
     end
   end
-
-  for i=1,4 do
-    if (modPoints[2*i] / love.graphics:getHeight()) > (modX / love.graphics:getHeight()) then
-      love.graphics.polygon("fill", getPoints(modX, modY - love.graphics:getHeight(), self.size/2, self.body:getAngle()))
-    elseif (modPoints[2*i] / love.graphics:getHeight()) < (modX / love.graphics:getHeight()) then
-      love.graphics.polygon("fill", getPoints(modX, modY + love.graphics:getHeight(), self.size/2, self.body:getAngle()))
-    end
-  end
-
-  love.graphics.polygon("fill", getPoints(modX, modY, self.size/2, self.body:getAngle()))
 end
 
 function getModular(body)
@@ -45,8 +33,3 @@ function getPoints(x, y, s, r)
   d = vector(-s, s):rotated(r) + vector(x, y)
   return a.x, a.y, b.x, b.y, c.x, c.y, d.x, d.y
 end
-
---  love.graphics.rectangle("fill", self.body:getX() - self.size/2, self.body:getY() - self.size/2, self.size, self.size)
--- love.graphics.rectangle("fill", (self.body:getX() - self.size/2) % love.graphics:getWidth(), self.body:getY() - self.size/2, self.size, self.size)
---  love.graphics.rectangle("fill", self.body:getX() - self.size/2, (self.body:getY() - self.size/2) % love.graphics:getHeight(), self.size, self.size)
--- love.graphics.rectangle("fill", (self.body:getX() - self.size/2) % love.graphics:getWidth(), (self.body:getY() - self.size/2) % love.graphics:getHeight(), self.size, self.size)
